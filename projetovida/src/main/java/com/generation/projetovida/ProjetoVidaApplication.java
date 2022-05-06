47d752b3349e49748b4aafc06d9ea33589940295
Fernando Brocco <fernandobroccox1@gmail.com>
2022-05-06 17:22:40 -0300

    Atualização

--- projetovida/src/main/java/com/generation/projetovida/ProjetoVidaApplication.java
+++ projetovida/src/main/java/com/generation/projetovida/ProjetoVidaApplication.java
@@ -1,4 +1,4 @@
-package com.generation.projetovida; 
+package com.generation.projetovida;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
