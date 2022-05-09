36ad78faa9cec0957e40ccc258df952719d1f845
DoubleKang1 <rafaeldalves@outlook.com>
2022-05-09 10:24:26 -0300

    Criação da tabela Usuario

---
+++ projetovida/src/main/java/com/generation/projetovida/model/Usuario.java
@@ -0,0 +1,107 @@
+package com.generation.projetovida.model;
+
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+		
+		@Id // Definir a coluna de id como chave primaria
+		@GeneratedValue(strategy = GenerationType.IDENTITY) // Equivalente ao auto_increment no mysql
+		private Long id;
+
+		@NotBlank // Define que o campo não pode ter espaços vazios e em branco
+		private String nome;
+
+		@NotBlank
+		private String email_usuario;
+		
+		@NotNull // Define que o campo não pode ter espaços vazios
+		@Size (min=11, max=11)
+		private int cpf;
+		
+		@NotBlank
+		private String data_nascimento;
+		
+		@NotBlank
+		private String senha;
+		
+		@NotNull
+		private boolean voluntario;
+		
+		@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL) // Um usuário para vários produtos
+		@JsonIgnoreProperties("usuario") // recursividade - Ignora o termo usuário na busca
+		private List<Produto> produto;
+		
+		//Getters and Setters - Permite que outras classes podem intereragir com os atributos privados dessa classe
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getEmail_usuario() {
+			return email_usuario;
+		}
+
+		public void setEmail_usuario(String email_usuario) {
+			this.email_usuario = email_usuario;
+		}
+
+		public int getCpf() {
+			return cpf;
+		}
+
+		public void setCpf(int cpf) {
+			this.cpf = cpf;
+		}
+
+		public String getData_nascimento() {
+			return data_nascimento;
+		}
+
+		public void setData_nascimento(String data_nascimento) {
+			this.data_nascimento = data_nascimento;
+		}
+
+		public String getSenha() {
+			return senha;
+		}
+
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+
+		public boolean isVoluntario() {
+			return voluntario;
+		}
+
+		public void setVoluntario(boolean voluntario) {
+			this.voluntario = voluntario;
+		}
+		
+}
